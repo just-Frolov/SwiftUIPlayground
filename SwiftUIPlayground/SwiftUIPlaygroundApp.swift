9dc7e1394b5f09803b307c1cb077db5057a71122
Danil <frolovdanil75@gmail.com>
2023-10-17 22:19:02 +0400

    added xcode project

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Danil Frolov on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
